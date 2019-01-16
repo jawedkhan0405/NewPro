221e12a7fd7d6fa017f1fa7799f2dcf4656378dd
jawed <jawedkhan0405@gmail.com>
2019-01-16 13:55:53 +0530

    file with radio

---
+++ displaydata.java
@@ -0,0 +1,13 @@
+package com.example.arjun.newpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displaydata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaydata);
+    }
+}
